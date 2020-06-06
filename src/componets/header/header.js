c22df7c8587f680601c5e38237741c00cb0cf9cb
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-06 15:47:47 -0500

    refactor: Add header.css and header.js

---
+++ src/componets/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return(
+    <header>
+      <h1>RICK AND MORTY</h1>
+      <p>GET API</p>
+    </header>
+  )
+}
+
+export default Header;
