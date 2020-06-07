3dae67eb0721264d4cdcff18e2170a8e8b8ff548
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-06 22:54:51 -0500

    Updates

---
+++ precache-manifest.a42d5cddcdb0a7de84added93dd55184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55f972f7c73b691a2a783135b817f50",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "e938f9a1e0365d848c22",
+    "url": "/rick-and-morty/static/css/main.a579848e.chunk.css"
+  },
+  {
+    "revision": "0a275b25b4cc65d70fcf",
+    "url": "/rick-and-morty/static/js/2.2faa459c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rick-and-morty/static/js/2.2faa459c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e938f9a1e0365d848c22",
+    "url": "/rick-and-morty/static/js/main.79d058cb.chunk.js"
+  },
+  {
+    "revision": "f50e5a98051c67529667",
+    "url": "/rick-and-morty/static/js/runtime-main.8e5a01b9.js"
+  }
+]);
